0674c631a5ffd5dcd57795bafd3d2f68c5b57645
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-06-21 16:18:57 -0400

    restful paths complete

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const transactions = require("./controllers/transactionsController.js");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/transactions", transactions)
+app.use("/transactions", transactions);
 
-// ROOT
 app.get("/", (req, res) => {
   res.send("Basic Express App - ROOT");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
 
 module.exports = app;
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,18 +1,37 @@
 const transactions = require("express").Router();
 const transactionsArray = require("../models/transactions");
 
-transactions.get("/:id", (req, res) => {
-  res.send("Basic Express App - ROOT");
-});
-
 transactions.get("/", (req, res) => {
   res.send(transactionsArray);
 });
 
-transactions.post("/", (req, res) => {
-  const { body } = req;
-  res.json(transactionsArray.push(body));
+transactions.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if (transactionsArray[id]) {
+    res.json(transactionsArray[id]);
+  } else {
+    res.redirect("/404");
+  }
 });
 
+transactions.post("/", (req, res) => {
+  const { body } = req;
+  transactionsArray.push(body);
+  const newTransaction = transactionsArray.length - 1;
+  res.json(transactionsArray[newTransaction]);
+});
+
+transactions.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  transactionsArray[id] = body;
+  res.json(transactionsArray[id]);
+});
+
+transactions.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedTransaction = transactionsArray.splice(id, 1);
+  res.json(deletedTransaction[0]);
+});
 
 module.exports = transactions;
